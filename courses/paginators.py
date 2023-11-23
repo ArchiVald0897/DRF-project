eb9542244bef4a542cd3b5dc4f217b08eb656789
sergei valdemarov <valdemarovsergei@gmail.com>
2023-11-23 22:26:50 +0300

    Commit5

--- courses/paginators.py
+++ courses/paginators.py
@@ -2,12 +2,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CoursePaginator(PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_size_query_param = 'page_size'
-    max_page_size = 30
+    max_page_size = 15
 
 
 class LessonPaginator(PageNumberPagination):
-    page_size = 7
+    page_size = 5
     page_size_query_param = 'page_size'
-    max_page_size = 30
+    max_page_size = 15
